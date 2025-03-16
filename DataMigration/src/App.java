36e78b1f44a348039436efd1587ba12da9aaeecb
Remiliano <emilianoprestesdacosta@proton.me>
2025-03-16 08:10:45 -0300

    add dependencies and libraries to begin working with apache POI

--- DataMigration/src/App.java
+++ DataMigration/src/App.java
@@ -1,3 +1,11 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+//I'll finish importing all the dependencies later ¬¬
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
