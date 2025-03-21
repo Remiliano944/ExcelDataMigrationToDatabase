a0a25cab07f0117a60cfe300ef378fcedaba460a
Remiliano944 <emilianoprestesdacosta@proton.me>
2025-03-21 08:27:52 -0300

    began implementation of apache POI + gitignore

--- DataMigration/src/App.java
+++ DataMigration/src/App.java
@@ -1,14 +1,16 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-//I'll finish importing all the dependencies later ¬¬
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("mmmmmmmmmmm");
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        XSSFSheet sheet = workbook.createSheet("test");
     }
 }
