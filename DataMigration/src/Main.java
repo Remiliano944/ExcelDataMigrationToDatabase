c83f7ffd996de91cc784778a9b62aa96b245fe82
Remiliano944 <emilianoprestesdacosta@proton.me>
2025-03-23 10:34:47 -0300

    Migration is done.

---
+++ DataMigration/src/Main.java
@@ -0,0 +1,68 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Iterator;
+import org.apache.poi.ss.usermodel.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //bloco try para detectar erros
+        try(InputStream file = new FileInputStream("data.xlsx")){
+            //instanciar o workbook usando o elemento obtido do FileInputStream. Isso implica que o programa simplesmente traduz com base na dedução do que o objeto da classe FileInputStream possui? Se sim. Wow.
+
+           // XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            //Vou tentar implementar por meio do WorkbookFactory. Já que, segundo a documentação, ele já cuida de tratar o tipo do arquivo.
+            Workbook workbook = WorkbookFactory.create(file);
+
+            //Agora sim vamos para as tabelinhas.
+
+            Sheet sheet = workbook.getSheetAt(0);
+
+            //iterar em cada tabela. Um por um
+
+            //vamos implementar um loop while para simplificar.
+
+            //Enquanto houver dados ou conteúdo na coluna. Continue :)
+            //Melhor ainda. Para cada dado que existir no limite da coleção.
+            for (Row row : sheet) {
+
+                //uma vez extraido os dados da coluna. Vamos para as linhas afim de obter suas informações.
+
+                Iterator<Cell> cellIterator = row.cellIterator();
+
+                //enquanto houver dados ou conteúdo na linha. Continue :)
+
+
+                while (cellIterator.hasNext()) {
+
+                    //obtendo os dados da linha
+
+                    Cell cell = cellIterator.next();
+                    //como o programa pode obter vários tipos de informações (sendo estas numéricas ou alfabéticas)...
+
+
+                    switch (cell.getCellType()) {
+                        //Caso sejam letras/alfabético
+                        case STRING:
+                            System.out.print(cell.getStringCellValue() + "t");
+                            break;
+                        case NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "t");
+                            break;
+                        default:
+                            System.out.println("Erro");
+                            throw new Exception("Algum valor fora de cogitação foi detectado");
+                    }
+                }
+                System.out.println(" ");
+            }
+            file.close();
+            workbook.close();
+        }
+        catch (Exception e ){
+            //coletar a exceção para que eu possa lidar com ela mais tarde.
+            e.printStackTrace();
+        }
+    }
+}
