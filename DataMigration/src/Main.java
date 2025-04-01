473dd165ca4c09a6e30b1fabe95d4d79639e43b9
Remiliano944 <emilianoprestesdacosta@proton.me>
2025-04-01 09:12:31 -0300

    programa funcionando com formulas. E convertendo o resultado para inteiros com no máximo 7 digitos.

--- DataMigration/src/Main.java
+++ DataMigration/src/Main.java
@@ -18,6 +18,10 @@ public class Main {
             //aparentemente é falta de dependências. Oh... Agora sim o uso do maven faz sentido!! Agora eu entendi!!
             Workbook workbook = WorkbookFactory.create(file);
 
+            //declarando FormulaEvaluator para poder trabalhar com formulas em Excel.
+            FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
+            //O workbook retorna um objeto da classe CreationHelper, que possui em seus métodos a capacidade de retornar um objeto da classe FormulaEvaluator
+
             //Agora sim vamos para as tabelinhas.
 
             Sheet sheet = workbook.getSheetAt(0);
@@ -48,6 +52,26 @@ public class Main {
                             break;
                         default:
                             //para fazer forma de ler formulas de excel.
+                            //O exemplo na documentação do POI sugere usar o evaluator antes do switch case, para fins de poder já tratar do objeto.
+                            //Oque faz sentido mediante o fato de que o evaluator não faz alterações ou ações em caso de dados que não sejam formulas...
+                            //Mas ei, oque aconteceria se eu usasse o Evaluator aqui huh?
+                            //Aparentemente nada demais, o evaluator opera tranquilamente, e não só isso como também eu entendi que o evaluator RETORNA um CellValue.
+                            //Que basicamente opera como um Cell normal :)
+                            //Agora só preciso reduzir a quantidade de digitos no número da formula para no máximo 7 digitos
+
+                            //Obter valor da celula e convertê-lo para inteiro
+                            int valor = (int)evaluator.evaluate(cell).getNumberValue();
+                            //captar a largura de digitos do valor
+                            int length = String.valueOf(valor).length();
+
+                            //rodar em looping para que enquanto a largura for maior do que o limite permitido (neste caso 7) dividir o número por 10.
+                            while(length > 7){
+                                valor /= 10;
+                                //O numero perde uma casa decimal, e por ser um inteiro, a informação "perdida" não é contada, oque realmente implica que o número perdeu o digito
+                                length--;
+                            }
+                            System.out.println(valor + ", ");
+                            break;
                     }
                 }
                 System.out.println(" ");
